639251f29ac6a3a40fa45ff3cf9e61f063c8d959
Noman <shahmubinalnoman45@gmail.com>
2023-09-24 10:25:23 +0600

    Add files via upload

---
+++ Trail.java
@@ -0,0 +1,30 @@
+public class Trail{
+    public static void main(String[] args) {
+       int grade = 90
+       ;
+
+switch (grade) {
+  case 90:
+    System.out.println("You got an A!");
+    break;
+  case 80:
+    System.out.println("You got a B!");
+    break;
+  case 70:
+    System.out.println("You got a C!");
+    break;
+  case 60:
+    System.out.println("You got a D!");
+    break;
+  default:
+    System.out.println("You failed!");
+} 
+    }
+}
+
+
+
+
+
+
+
