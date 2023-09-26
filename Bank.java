00a05c91ad64077b39c56a4d80fac2c7b495b9ed
Java-Programming <shahmubinalnoman45@gmail.com>
2023-09-26 15:39:28 +0600

    #

--- Bank.java
+++ Bank.java
@@ -1,76 +1,21 @@
 class BankAccount {
-    private int accountNumber;
-    private String accountHolderName;
-    private double balance;
+    int accountNumber;
+    String accountHolderName;
+    double balance;
 
-    public BankAccount(int accountNumber, String accountHolderName, double initialDeposit) {
-        this.accountNumber = accountNumber;
-        this.accountHolderName = accountHolderName;
-        this.balance = initialDeposit;
-    }
-
-    public int getAccountNumber() {
-        return accountNumber;
-    }
-
-    public String getAccountHolderName() {
-        return accountHolderName;
-    }
-
-    public double getBalance() {
-        return balance;
-    }
-
-    public void deposit(double amount) {
-        if (amount > 0) {
-            balance += amount;
-            System.out.println("Deposited: $" + amount);
-        } else {
-            System.out.println("Invalid deposit amount.");
-        }
-    }
-
-    public void withdraw(double amount) {
-        if (amount > 0 && amount <= balance) {
-            balance -= amount;
-            System.out.println("Withdrawn: $" + amount);
-        } else {
-            System.out.println("Invalid withdrawal amount.");
-        }
-    }
-}
-
-class AccountHolder {
-    private int accountNumber;
-    private String accountHolderName;
-    private double depositedMoney;
-
-    public AccountHolder(int accountNumber, String accountHolderName, double depositedMoney) {
-        this.accountNumber = accountNumber;
-        this.accountHolderName = accountHolderName;
-        this.depositedMoney = depositedMoney;
-    }
-
-    public int getAccountNumber() {
-        return accountNumber;
-    }
-
-    public String getAccountHolderName() {
-        return accountHolderName;
-    }
-
-    public double getDepositedMoney() {
-        return depositedMoney;
+    BankAccount(int accNumber, String accHolderName, double initialDeposit) {
+        accountNumber = accNumber;
+        accountHolderName = accHolderName;
+        balance = initialDeposit;
     }
 }
 
 public class Bank {
     public static void main(String[] args) {
-        BankAccount bankAccount = new BankAccount(12345, "John Doe", 1000.0);
-        AccountHolder accountHolder = new AccountHolder(bankAccount.getAccountNumber(), bankAccount.getAccountHolderName(), bankAccount.getBalance());
+        BankAccount account = new BankAccount(12345, "Shah Mubin Al Noman", 1000.0);
 
-        System.out.println("Account Number: " + accountHolder.getAccountNumber());
-        System.out.println("Account Holder Name: " + accountHolder.getAccountHolderName());
-        System.out.println("Deposited Money: $" + accountHolder.getDepositedMoney());
+        System.out.println("Account Number: " + account.accountNumber);
+        System.out.println("Account Holder Name: " + account.accountHolderName);
+        System.out.println("Balance: $" + account.balance);
     }
 }
