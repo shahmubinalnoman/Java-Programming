a1229c27ed337d5f4453a8e430f444826f7bdcde
Noman <shahmubinalnoman45@gmail.com>
2023-10-08 23:35:18 +0600

    Lab1

--- Student.java
+++ Student.java
@@ -4,7 +4,6 @@ public class Student {
         Main1 s1 = new Main1("Rabbi", "Rajshahi", 22);
         Main1 s2 = new Main1("Shihab", "Dhaka", 18);
 
-    
         System.out.println("Student 1 Data:");
         s1.printData();
 
@@ -12,24 +11,22 @@ public class Student {
         s2.printData();
     }
 }
+
 class Main1 {
 
     String name;
     String city;
     int age;
 
-
     public Main1(String name, String city, int age) {
-         this.name = name;
+        this.name = name;
         this.city = city;
         this.age = age;
     }
 
-    
     public void printData() {
         System.out.println("Student Name: " + name);
         System.out.println("City: " + city);
         System.out.println("Age: " + age);
     }
 }
-
